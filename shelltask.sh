1432121f443e9338618384965b61b58ba43a247e
sweta <sweta@gmail.com>
2024-10-20 10:40:48 +0000

    Added shelltask.sh file

---
+++ shelltask.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+echo "Enter first number"
+read a
+echo "Enter second number"
+read b
+echo "Enter the arithmatic operation"
+read operator
+if [ $operator = sum ]; then
+echo "$((a+b))"
+elif [ $operator = sub ];then
+echo "$((a-b)))"
+elif [ $operator = mul ];then
+echo "$((a*b))"
+fi
+
